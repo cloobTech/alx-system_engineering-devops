7ed5da78285314eeca57294256911da1d7d51961
cloobTech <belkid98@gmail.com>
2023-04-24 14:23:09 -0700

    save

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Python script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress.
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employee_id = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com"
+
+    employee = requests.get('{}/users/{}'.format(url, employee_id)).json()
+    tasks = requests.get('{}/todos'.format(url)).json()
+
+    EMPLOYEE_NAME = employee.get('username')
+    task_data = []
+
+    for task in tasks:
+        if task.get('userId') == employee_id:
+            task_data.append(task)
+
+    json_data = {str(employee_id): task_data}
+    output_file = '{}.json'.format(employee_id)
+
+    with open(output_file, 'w') as jsonfile:
+        json.dump(json_data, jsonfile)
